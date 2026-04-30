7e5b4211fecbd3f03518f1892acf6aac3ade9e33
Four <fourm6348@gmail.com>
2026-04-30 23:37:22 +0700

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,5 +1,6 @@
+import discord
+import os  # <--- ต้องมีบรรทัดนี้!
 from discord import app_commands
-from discord.ext import commands
 from datetime import datetime
 from flask import Flask
 from threading import Thread
